169f31333c1124aa3cc2b7dbae260eac51b54885
Achmad Faiz Alawi <achmad.alawi@gmail.com>
2023-02-09 19:45:17 +0700

    Task Collection

---
+++ bin/week_2/collection_number2.dart
@@ -0,0 +1,48 @@
+//Task Number 2
+void main(){
+  //Inisialisasi variabel List dan nilai masukan pada fungsi 'newList'
+  final List listNested = newList(2, 2);
+  //Inisialisasi variabel List untuk menampung data fungsi 'listMakanan'
+  late List makanan; 
+  //Inisialisasi nilai masukan kedalam fungsi 'listMakanan'
+  makanan = listMakanan(listNested, 0, 0, "Mie Rebus");
+  makanan = listMakanan(listNested, 0, 1, "Mie Goreng");
+  makanan = listMakanan(listNested, 1, 0, "Ayam Bakar");
+  makanan = listMakanan(listNested, 1, 1, "Ayam Goreng");
+  //Menampilkan hasil setelah memberikan nilai masukan pada List 'makanan'
+  print("List yang dibuat: ");
+  print(makanan);
+
+  //Inisialisasi variabel bertipe data Map
+  late final Map mapMakanan = Map ();
+  //Memberikan nilai masukan kedalam Map 'mapMakanan' dari List 'makanan'
+  mapMakanan["Olahan Mie"] = makanan[0];
+  mapMakanan["Olahan Ayam"] = makanan[1];
+  //Menampilkan hasil setelah memberikan nilai masukan pada Map 'mapMakanan'
+  print("\nMap yang dibuat: ");
+  print(mapMakanan);
+}
+
+//Membuat list multidimensi dengan parameter jumlah sublist dan jumlah elemen didalam sublist
+List newList(final int jumlahSubList, final int jumlahElementSubList){
+  /*
+  'jumlahSubList' = untuk membuat banyaknya sublist pada List.
+  'jumlahElementSubList' = untuk membuat banyaknya element didalam sublist pada List.
+  "null" hanya sebagai parameter untuk menentukan jenis tipe data element didalam sublist.
+  */
+  var tList = List.generate(jumlahSubList, (i) => List.generate(jumlahElementSubList, (i) => "null", growable: true), growable: true);
+  return tList;
+}
+
+//Menambahkan nilai masukan pada element setiap sublist
+List listMakanan(final List inputList, final int indeksData, final int indeksSubData, final String nilaiMasukan){
+  /*
+  'inputList' = sebagai pemanggil List yang akan diberi nilai masukan.
+  'indeksData' = sebagai pemanggil indeks sublist mana yang akan diberi nilai masukan.
+  'indeksSubData' = sebagai pemanggil indeks element sublist mana yang akan diberi nilai masukan.
+  'nilaiMasukan' = sebagai tempat untuk menyimpan nilai masukan.
+  */
+  final List listBaru = inputList;
+  listBaru[indeksData][indeksSubData] = nilaiMasukan;
+  return listBaru;
+}
