a0467d8b50acf2ac381376cd315704c4efbb40be
Achmad Faiz Alawi <achmad.alawi@gmail.com>
2023-02-16 08:58:00 +0700

    Task Collection

--- bin/week_2/7_oop2/no3_no4/oop2_no3_no4_matematika.dart
+++ bin/week_2/7_oop2/no3_no4/oop2_no3_no4_matematika.dart
@@ -1,5 +1,8 @@
+//Deklarasi class 'Matematika' sebagai abstract class agar tidak dapat diakses secara langsung.
 abstract class Matematika{
+  //Deklarasi method 'hasil()' sebagai tempat untuk memproses perhitungan di subclass saat class ini diimplements.
   int hasil(){
+    //Return nilai '0' sebagai return sementara, return ini akan diganti di subclass menyesuaikan perhitungannya.
     return 0;
   }
 }
